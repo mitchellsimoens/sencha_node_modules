df7525cfd02660629b7509a7a251720e6809f580
Mitchell Simoens <mitchellsimoens@gmail.com>
2017-08-09 13:35:36 -0400

    Add in nsp checking

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,9 @@
 PACKAGE="*"
 
 help_menu() {
-  echo "Usage:
+    echo "Usage:
 
-  ${0##*/} [-h][-c][-b]
+${0##*/} [-h][-c][-b][-n]
 
 Options:
 
@@ -17,68 +17,83 @@ Options:
 
   -b, --break
     enable debug by breaking
+
+  -n, --nsp
+    enable the nsp vulnerability check
   "
 }
 
 parse_options() {
-  case $opt in
-    h|help)
-      help_menu
-      exit
-      ;;
-    c|coverage)
-      COVERAGE=true
-      ;;
-    b|break)
-      BREAK=true
-      ;;
-    sencha-*)
-      PACKAGE=$opt
-      ;;
-    *)
-      echo "Unknown option: ${opt}"
-      echo "Run ${0##*/} -h for help.">&2
-      echo
-      exit 1
-  esac
+    case $opt in
+        h|help)
+            help_menu
+            exit
+            ;;
+        c|coverage)
+            COVERAGE=true
+            ;;
+        b|break)
+            BREAK=true
+            ;;
+        n|nsp)
+            NSP=true
+            ;;
+        sencha-*)
+            PACKAGE=$opt
+        ;;
+        *)
+            echo "Unknown option: ${opt}"
+            echo "Run ${0##*/} -h for help.">&2
+            echo
+            exit 1
+    esac
 }
 options=$@
 
 until [ "$options" = "" ]; do
-  if [[ $options =~ (^ *(--([a-zA-Z0-9-]+)|-([a-zA-Z0-9-]+))(( |=)(([\_\.\?\/\\a-zA-Z0-9]?[ -]?[\_\.\?a-zA-Z0-9]+)+))?(.*)|(.+)) ]]; then
-    if [[ ${BASH_REMATCH[3]} ]]; then # for --option[=][attribute] or --option[=][attribute]
-      opt=${BASH_REMATCH[3]}
-      attr=${BASH_REMATCH[7]}
-      options=${BASH_REMATCH[9]}
-    elif [[ ${BASH_REMATCH[4]} ]]; then # for block options -qwert[=][attribute] or single short option -a[=][attribute]
-      pile=${BASH_REMATCH[4]}
-      while (( ${#pile} > 1 )); do
-        opt=${pile:0:1}
-        attr=""
-        pile=${pile/${pile:0:1}/}
+    if [[ $options =~ (^ *(--([a-zA-Z0-9-]+)|-([a-zA-Z0-9-]+))(( |=)(([\_\.\?\/\\a-zA-Z0-9]?[ -]?[\_\.\?a-zA-Z0-9]+)+))?(.*)|(.+)) ]]; then
+        if [[ ${BASH_REMATCH[3]} ]]; then # for --option[=][attribute] or --option[=][attribute]
+            opt=${BASH_REMATCH[3]}
+            attr=${BASH_REMATCH[7]}
+            options=${BASH_REMATCH[9]}
+        elif [[ ${BASH_REMATCH[4]} ]]; then # for block options -qwert[=][attribute] or single short option -a[=][attribute]
+            pile=${BASH_REMATCH[4]}
+
+            while (( ${#pile} > 1 )); do
+                opt=${pile:0:1}
+                attr=""
+                pile=${pile/${pile:0:1}/}
+                parse_options
+            done
+
+            opt=$pile
+            attr=${BASH_REMATCH[7]}
+            options=${BASH_REMATCH[9]}
+        else # leftovers that don't match
+            opt=${BASH_REMATCH[10]}
+            options=""
+        fi
+
         parse_options
-      done
-      opt=$pile
-      attr=${BASH_REMATCH[7]}
-      options=${BASH_REMATCH[9]}
-    else # leftovers that don't match
-      opt=${BASH_REMATCH[10]}
-      options=""
     fi
-    parse_options
-  fi
 done
+
+if [ $NSP ]; then
+    echo "Checking vulnerabilities with nsp (https://www.npmjs.com/package/nsp)"
+    npm run nsp
+    echo
+fi
 
 if [ $COVERAGE ]; then
     if [ $BREAK ]; then
-      nyc mocha --debug-brk --inspect "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
+        nyc mocha --inspect-brk "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
     else
-      nyc mocha "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
+        nyc mocha "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
     fi
 else
     if [ $BREAK ]; then
-      mocha --debug-brk --inspect "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
+        mocha --no-timeouts --inspect-brk "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
     else
-      mocha "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
+        mocha --no-timeouts "test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.helper.js" "packages/${PACKAGE}/test/**/*.spec.js"
     fi
 fi
