1fc5cba66dab4ead9b9f74b07acd51e8520117b1
Mitchell Simoens <mitchellsimoens@gmail.com>
2017-08-10 10:19:26 -0400

    Add in update-notifier module

--- packages/sencha-deploy/bin/sencha_deploy.js
+++ packages/sencha-deploy/bin/sencha_deploy.js
@@ -6,12 +6,23 @@ const {
     util  : { Logger, Shutdown }
 } = require('../');
 
+const updater = require('update-notifier');
+const pkg     = require('../package.json');
+
+const notifier = updater({
+    pkg
+});
+
+notifier.notify({
+    defer : true
+});
+
 const app = new App();
 
 Logger.init(app.config.logger.level)
-    .then(app.getArguments     .bind(app))
+    .then(app.getArguments     .bind(app)) // eslint-disable-line no-whitespace-before-property
     .then(app.createConnections.bind(app))
-    .then(app.run              .bind(app))
+    .then(app.run              .bind(app)) // eslint-disable-line no-whitespace-before-property
     .then(() => {
         const log = app.getArgument('log');
 
